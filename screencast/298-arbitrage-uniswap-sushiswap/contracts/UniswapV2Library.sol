9b5e49c5459eedfee6156b240c460cdfbaaed282
Moifadi <46161809+The0Leeuwfas0Hippy@users.noreply.github.com>
2021-04-19 12:17:15 -0800

    Update UniswapV2Library.sol

--- screencast/298-arbitrage-uniswap-sushiswap/contracts/UniswapV2Library.sol
+++ screencast/298-arbitrage-uniswap-sushiswap/contracts/UniswapV2Library.sol
@@ -1,10 +1,9 @@
 pragma solidity >=0.5.0;
 
-//import './interfaces/IUniswapV2Pair.sol';
-import './SafeMath.sol';
-//import "@openzeppelin/contracts/math/SafeMath.sol";
+import './interfaces/IUniswapV2Pair.sol';
+import '.math/SafeMath.sol';
 
-//import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v2.5.0/contracts/math/SafeMath.sol'
+//import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v2.5.0/contracts/math/SafeMath.sol';
 
 library UniswapV2Library {
     using SafeMath for uint;
