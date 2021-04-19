47a08755622792dd7b2c1f62c71800bb556a711b
Moifadi <46161809+The0Leeuwfas0Hippy@users.noreply.github.com>
2021-04-19 12:20:35 -0800

    Update UniswapV2Library.sol

--- screencast/298-arbitrage-uniswap-sushiswap/contracts/UniswapV2Library.sol
+++ screencast/298-arbitrage-uniswap-sushiswap/contracts/UniswapV2Library.sol
@@ -1,7 +1,7 @@
 pragma solidity >=0.5.0;
 
 import './interfaces/IUniswapV2Pair.sol';
-import '.math/SafeMath.sol';
+import './math/SafeMath.sol';
 
 library UniswapV2Library {
     using SafeMath for uint;
